6bd31d44abea72da15c7a997b4a358612894735d
robertkrimen <robertkrimen@gmail.com>
2009-05-14 20:30:17 -0700

    Include Carp::Clan dependency in Makefile.PL

--- lib/Diff/LibXDiff.pm
+++ lib/Diff/LibXDiff.pm
@@ -9,11 +9,11 @@ Diff::LibXDiff - Calculate a diff with LibXDiff (via XS)
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
