527ab1e1d6b5bdccf8226a6c0c2598af9513f2e2
robertkrimen <robertkrimen@gmail.com>
2009-05-16 00:46:02 -0700

    Added Test::Most dependency

--- lib/Diff/LibXDiff.pm
+++ lib/Diff/LibXDiff.pm
@@ -9,11 +9,11 @@ Diff::LibXDiff - Calculate a diff with LibXDiff (via XS)
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
